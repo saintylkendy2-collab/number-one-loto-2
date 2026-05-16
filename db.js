621c120291d99dc0ddf913d33a97f6b22ac0688a
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-16 09:52:43 -0400

    separate mongo

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect("mongodb+srv://admin:1234@cluster0.yzqmfuc.mongodb.net/loto?retryWrites=true&w=majority");
+    await mongoose.connect(process.env.MONGO_URI)
     console.log("✅ MongoDB connecté");
   } catch (err) {
     console.error("❌ MongoDB error:", err);
