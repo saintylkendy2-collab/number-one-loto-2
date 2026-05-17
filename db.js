9e6081d6815a2096eaba8134df60f714a59fa3c8
kendy saintyl <kendysaintyl@MBP-de-kendy.lan>
2026-05-16 20:22:16 -0400

    update

--- db.js
+++ db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect(
+  process.env.MONGO_URI || "mongodb://127.0.0.1:27017/number_one_loto_2"
+);
     console.log("✅ MongoDB connecté");
   } catch (err) {
     console.error("❌ MongoDB error:", err);
